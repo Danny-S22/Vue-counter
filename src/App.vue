74e3e00ab5a18f7142d88610c06ec63b6fee7b43
Danny <byxdenpro@gmail.com>
2024-07-03 12:26:57 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <counter></counter>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import counter from './components/counter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    counter
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: "Anton SC", sans-serif;
+  font-weight: 400;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
---
+++ src/components/counter.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <h1>Vue Counter</h1>
+        <p>{{ count }}</p>
+        <button @click="increment">Increment</button>
+        <button @click="decrement">Decrement</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Counter',
+        data() {
+            return {
+                count: 0
+            }
+        },
+        methods: {
+            increment() {
+                this.count++
+            },
+            decrement() {
+                this.count--
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.container {
+    text-align: center;
+    color: #2c3e50;
+    margin: 0 auto;
+}
+</style>
