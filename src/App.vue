305e76db96db110d66b65881abb7b5e38e654d56
Danny <byxdenpro@gmail.com>
2024-07-03 15:30:49 +0300

    Add a modal functionality

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,38 @@
 <template>
   <div id="app">
+    <button class="btn btn-primary btn-dark modal-btn" @click="toggleModal">Open Modal</button>
     <counter />
+    
+    <Modal :modalActive="modalActive" @close-modal="toggleModal"> 
+      <h1>Counter App</h1>
+      <p>
+        The counter is a simple application built with Vue.js, designed to increment or decrement a value with button clicks. 
+        The counter value is saved in the browser's local storage. This application was created to practice Vue.js development.</p>
+    </Modal>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
 import counter from './components/counter.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
   components: {
-    counter
+    counter,
+    Modal
+  },
+  setup() {
+    const modalActive = ref(false);
+    const toggleModal = () => {
+      modalActive.value = !modalActive.value;
+    }
+
+    return {
+      modalActive,
+      toggleModal
+    };
   }
 }
 </script>
@@ -19,5 +41,12 @@ export default {
 #app {
   font-family: "Anton SC", sans-serif;
   font-weight: 400;
+}
+
+.modal-btn {
+  margin: 2rem;
+  position: absolute;
+  top: 0;
+  left: 0;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="modal" v-show="modalActive">
+        <div class="modal-content p-4" v-if="modalActive">
+            <slot />
+            <button class="btn btn-secondary btn-dark" @click="closeModal">Close</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineEmits, defineProps } from 'vue';
+
+const emit = defineEmits(['close-modal']);
+const props = defineProps({
+    modalActive: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const closeModal = () => {
+    emit('close-modal');
+};
+</script>
+
+<style scoped>
+.modal {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+.modal-content {
+    background-color: white;
+    border-radius: 5px;
+    max-width: 30vw;
+}
+</style>
